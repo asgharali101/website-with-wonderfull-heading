f250784634cbb02e47c253360f27762fd7301af9
asgharburdi <122790377+asgharburdi@users.noreply.github.com>
2023-04-05 11:15:32 +0500

    i hope everyone impress from my work

---
+++ tailwind.config.cjs
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html","./src/**/*.{js,ts,jsx,tsx}", "./node_modules/flowbite/**/*.js"],
+  theme: {
+    extend: {},
+  },
+  plugins: [require('@tailwindcss/forms'), require('flowbite/plugin')],
+}
